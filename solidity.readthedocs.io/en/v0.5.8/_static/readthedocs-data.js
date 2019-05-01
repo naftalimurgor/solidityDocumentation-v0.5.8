99e7bacdd8d5e5801adb9bdefda2c0a1635f065c
murgor <murgornaftali@gmail.com>
2019-05-01 15:06:35 +0300

    Initial commit

---
+++ solidity.readthedocs.io/en/v0.5.8/_static/readthedocs-data.js
@@ -0,0 +1,19 @@
+var READTHEDOCS_DATA = {
+    project: "solidity",
+    version: "v0.5.8",
+    language: "en",
+    programming_language: "cpp",
+    subprojects: {},
+    canonical_url: "https://solidity.readthedocs.io/en/v0.5.7/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/docs/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "23d335f2",
+    ad_free: false,
+
+    global_analytics_code: 'UA-17997319-1',
+    user_analytics_code: null
+};
+
